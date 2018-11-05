8c5e6cd535dbad120e62c56eb7537b607cd7cfa1
Paul Sandford <hopeforall1561@yahoo.com>
2018-11-05 04:53:10 -0600

    Functionality of addblog and singlepost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -24,10 +26,16 @@ class Blog(db.Model):
 def index():
     return redirect('/blog')
 
-@app.route('/blog')
-def blog():
-    blogs = Blog.query.all()
+
+@app.route('/blog', defaults={'blog_id' : 'All'})
+@app.route('/blog/<blog_id>')
+@app.route('/blog/<int:blog_id>')
+def blog(blog_id):
+    if blog_id != 'All':
+        blog = Blog.query.get(int(blog_id))
+        return render_template('singlepost.html',title=blog.title,blog=blog)
     
+    blogs = Blog.query.all()
     return render_template('blogs.html',title="Build-a-Blog", 
         blogs=blogs)
 
@@ -35,7 +43,7 @@ def blog():
 def addblog():
     return render_template('addblog.html',title="Build-a-Blog")
 
-@app.route('/newpost', methods=['POST'])
+@app.route('/newpost', methods=['POST', 'GET'])
 def add_blog():
     blog_title = request.form['title']
     blog_body = request.form['body']
@@ -46,13 +54,15 @@ def add_blog():
         title_error = "Give your blog a title"
     if blog_body == "":
         body_error = "Tell us something"
-    if body_error == "" or title_error == "":
+    if body_error != "" or title_error != "":
         return render_template("addblog.html",title="Build-a-Blog",body_error=body_error,title_error=title_error,blog_title=blog_title,blog_body=blog_body)
 
     new_blog = Blog(blog_title, blog_body)
     db.session.add(new_blog)
     db.session.commit()
-    return redirect('/blog')
+    #add a query request for redirect
+    blog = Blog.query.get(new_blog.id)
+    return render_template('singlepost.html',title=blog.title,blog=blog)
 
 @app.route('/delete-blog', methods=['POST'])
 def delete_blog():
